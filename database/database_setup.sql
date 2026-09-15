2b4fa4c092225eadd7e7adb6c83ccc355b83e05c
malaika-11 <m.isingizwe2@alustudent.com>
2026-09-15 18:45:28 +0200

    Comments added in sql script

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -3,26 +3,35 @@ CREATE DATABASE IF NOT EXISTS momo_analytics;
 USE momo_analytics;
 
 CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    phone_number VARCHAR(20) NOT NULL UNIQUE,
-    name VARCHAR(100) NOT NULL
+    user_id INT PRIMARY KEY AUTO_INCREMENT -- 'Unique identifier for each user',
+
+    phone_number VARCHAR(20) NOT NULL UNIQUE -- 'Unique mobile phone number registered to the user',
+
+    name VARCHAR(100) NOT NULL -- 'Full name of the mobile money user'
 );
 
 
 CREATE TABLE transaction_categories (
-    category_id INT PRIMARY KEY AUTO_INCREMENT,
-    category_name VARCHAR(50) NOT NULL UNIQUE,
-    description VARCHAR(255)
+    category_id INT PRIMARY KEY AUTO_INCREMENT -- 'Unique identifier for each transaction category',
+
+    category_name VARCHAR(50) NOT NULL UNIQUE -- 'Name of the transaction category such as TRANSFER or PAYMENT',
+
+    description VARCHAR(255) -- 'Brief explanation of what the transaction category represents'
 );
 
 
 CREATE TABLE transactions (
-    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
-    amount DECIMAL(15,2) NOT NULL,
-    transaction_time DATETIME NOT NULL,
-    processed_by VARCHAR(100) NOT NULL,
-    status VARCHAR(20) NOT NULL,
-    category_id INT NOT NULL,
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT -- 'Unique identifier for each transaction',
+
+    amount DECIMAL(15,2) NOT NULL -- 'Amount of money involved in the transaction',
+
+    transaction_time DATETIME NOT NULL --'Date and time when the transaction occurred',
+
+    processed_by VARCHAR(100) NOT NULL  -- 'System or staff member responsible for processing the transaction',
+
+    status VARCHAR(20) NOT NULL  -- 'Current processing status of the transaction',
+
+    category_id INT NOT NULL -- 'Identifier linking the transaction to its category',
 
     CONSTRAINT chk_transaction_amount
         CHECK (amount > 0),
@@ -37,9 +46,11 @@ CREATE TABLE transactions (
 
 
 CREATE TABLE transaction_participants (
-    transaction_id INT NOT NULL,
-    user_id INT NOT NULL,
-    role VARCHAR(20) NOT NULL,
+    transaction_id INT NOT NULL  -- 'Identifier of the transaction involving the user',
+
+    user_id INT NOT NULL  -- 'Identifier of the user participating in the transaction',
+
+    role VARCHAR(20) NOT NULL -- 'Role of the user in the transaction: sender or receiver',
 
     PRIMARY KEY (transaction_id, user_id),
 
@@ -59,11 +70,15 @@ CREATE TABLE transaction_participants (
 
 
 CREATE TABLE system_logs (
-    log_id INT PRIMARY KEY AUTO_INCREMENT,
-    transaction_id INT NOT NULL,
-    log_message VARCHAR(255) NOT NULL,
-    log_level VARCHAR(20) NOT NULL,
-    log_time DATETIME NOT NULL,
+    log_id INT PRIMARY KEY AUTO_INCREMENT -- 'Unique identifier for each system log entry',
+
+    transaction_id INT NOT NULL -- 'Identifier of the transaction associated with the log',
+
+    log_message VARCHAR(255) NOT NULL -- 'Description of the event or action recorded by the system',
+
+    log_level VARCHAR(20) NOT NULL -- 'Severity level of the log such as INFO, WARNING, or ERROR',
+
+    log_time DATETIME NOT NULL --'Date and time when the system event was recorded',
 
     CONSTRAINT fk_log_transaction
         FOREIGN KEY (transaction_id)
@@ -124,6 +139,7 @@ VALUES
 (30000.00, '2026-09-10 11:45:00', 'System', 'PENDING', 4),
 (15000.00, '2026-09-10 13:20:00', 'System', 'FAILED', 5);
 
+
 INSERT INTO transaction_participants
     (transaction_id, user_id, role)
 VALUES
@@ -142,10 +158,6 @@ VALUES
 (5, 5, 'SENDER'),
 (5, 1, 'RECEIVER');
 
-
--- ----------------------------
--- SYSTEM LOGS
--- ----------------------------
 
 INSERT INTO system_logs
     (transaction_id, log_message, log_level, log_time)
